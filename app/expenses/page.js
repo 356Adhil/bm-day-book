cd61ef8aadcf4c4da500061865dc228cde30746b
Adhil-Akbar <356adhil@gmail.com>
2024-11-01 15:04:39 +0530

    updated

--- app/expenses/page.js
+++ app/expenses/page.js
@@ -1,12 +1,13 @@
 // bm-day-book/app/pages/expense-entries.js
 "use client";
-
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 import axios from "axios";
 import Header from "../components/Header";
 import ExpensesForm from "../components/ExpensesForm";
 
 export default function ExpenseEntries() {
+  const router = useRouter();
   const [error, setError] = useState(null);
 
   const handleAddExpense = async (expenseData) => {
@@ -19,6 +20,7 @@ export default function ExpenseEntries() {
       });
       // Optionally, show a success message or clear the error
       setError(null);
+      router.push("/expenses-entries");
     } catch (error) {
       console.error("Error adding expense:", error);
       setError("Failed to add expense. Please try again.");
@@ -28,10 +30,8 @@ export default function ExpenseEntries() {
   return (
     <>
       <Header />
-      <div className="p-6">
-        <h1 className="text-2xl font-semibold mb-4">Add Expense</h1>
+      <div className="p-6 mb-10">
         <ExpensesForm onSubmit={handleAddExpense} />
-
         {error && <p className="text-red-500 mt-4">{error}</p>}
       </div>
     </>
