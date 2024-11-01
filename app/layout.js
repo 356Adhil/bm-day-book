83e3ce4ecfe8acbe61099039ce37600b4cce4396
Adhil-Akbar <356adhil@gmail.com>
2024-11-01 17:56:53 +0530

    updated

--- app/layout.js
+++ app/layout.js
@@ -20,6 +20,28 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/compass-regular.png" type="image/png" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/compass-regular.png" />
+        <meta name="theme-color" content="#000000" />
+
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-W5NP77JK87"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `  
+                window.dataLayer = window.dataLayer || [];  
+                function gtag(){dataLayer.push(arguments);}  
+                gtag('js', new Date());  
+                gtag('config', 'G-W5NP77JK87');  
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
